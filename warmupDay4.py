e2c9c243334f68cdd418bb18e8cb98934b6bed64
tristanrosa2001 <trissy.rosa@gmail.com>
2019-03-02 15:37:36 +0000

    Tristan RosA wARM UP DAY 4

--- warmupDay4.py
+++ warmupDay4.py
@@ -1,1 +1,20 @@
 # File name: warmupDay4.py
+from random import randint
+player = input("rock(r), paper(p) or scissors(s)?")
+print(player, "vs", end=" ")
+chosen = randint(1,3)
+if chosen == 3:
+    computer = "r"
+elif chosen == 1:
+    computer = "p"
+else:
+    computer = "s"
+print(computer)
+if player == computer:
+    print("Draw!")
+elif player == "r" and computer == "s":
+    print("Player Won!!!")
+elif player == "s" and computer == "r":
+    print("Computer Won!!")
+elif player == "p" and computer == "s":
+    print("Computer won")
