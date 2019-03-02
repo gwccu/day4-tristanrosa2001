3dc0b8708a52f94706fa97889e93b45318b5eb22
tristanrosa2001 <trissy.rosa@gmail.com>
2019-03-02 16:49:42 +0000

    Tristan problem Set

---
+++ problemsetDay4.py
@@ -0,0 +1,12 @@
+strength = 5
+print("Your Stregnth Is 5!")
+while strength <= 10:
+    print (strength)
+    strength += .3
+print("Wow Time for Another Level")
+stregnth = 100
+print("Your Stregnth is 100!!!!")
+while stregnth >= 10:
+    print (stregnth)
+    stregnth -= 5
+print("You have lost your stregnth, you must level up")
